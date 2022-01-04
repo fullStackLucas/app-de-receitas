789b46a87113959eebc7dff181857df9f9e3e9e1
viniciusantana <viniciusantana@yahoo.com.br>
2022-01-04 19:39:18 -0300

    Iniciando Tela de explorar

--- src/pages/ExplorarBebidas.js
+++ src/pages/ExplorarBebidas.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -9,6 +10,36 @@ function ExplorarBebidas() {
         title="Explorar Bebidas"
         btnAvaliable={ false }
       />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button
+          type="button"
+          data-testid="explore-by-ingredient"
+        >
+          Por Ingrediente
+        </button>
+      </Link>
+
+      <Link to="/???????">
+        <button
+          type="button"
+          data-testid="explore-by-area"
+        >
+          Por Local de Origem
+        </button>
+      </Link>
+
+      <Link to={ `/bebidas/${'***ID ALEATÓRIO'}` }>
+        {/*
+        Ao clicar no botão "Me Surpreenda!" da tela de explorar bebidas a
+        rota muda para a página de detalhes de uma bebida aleatória
+        obtida através do endpoint https://www.thecocktaildb.com/api/json/v1/1/random.php.> */}
+        <button
+          type="button"
+          data-testid="explore-surprise"
+        >
+          Me Surpreenda!
+        </button>
+      </Link>
       <Footer />
     </>
   );
