8282ee0e808f9054116a65a78b37b545809227d3
Lucas Pinheiro <lucas.pinheiror@gmail.com>
2022-01-05 14:32:43 -0300

    Adicionando mudanças antes de mergear

--- src/context/Provider.js
+++ src/context/Provider.js
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Context from './Context';
+import { getBuscaComidas } from '../service/GetComidas';
+import { getBuscaBebidas } from '../service/GetBebidas';
+// import { getRandonBebidas } from '../service/GetBebidas';
 
 function Provider({ children }) {
   const [isSearchAvaliable, toggleSearch] = useState(false);
   const [dataMeals, setDataMeals] = useState([]);
   const [dataDrinks, setDataDrinks] = useState([]);
+
+  useEffect(() => {
+    getBuscaComidas().then((response) => setDataMeals([...response]));
+    getBuscaBebidas().then((response) => setDataDrinks([...response]));
+  }, []);
 
   const contextValue = {
     isSearchAvaliable,
