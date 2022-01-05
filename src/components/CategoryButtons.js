25bfecbaf15aa5133b0a6db9582537c45525653a
fariasao <enzofarias2010@gmail.com>
2022-01-05 19:01:27 -0300

    requisito 29 e 30 fizemos nova requisicao implementamos logica para mudanca de filtro e para o retorno da pagina inicial

--- src/components/CategoryButtons.js
+++ src/components/CategoryButtons.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
-import { getCategoriaBebidas, getFilteredDrinks } from '../service/GetBebidas';
-import { getCategoriaComidas, getFilteredMeals } from '../service/GetComidas';
+import { getBuscaBebidas,
+  getCategoriaBebidas, getFilteredDrinks } from '../service/GetBebidas';
+import { getBuscaComidas,
+  getCategoriaComidas, getFilteredMeals } from '../service/GetComidas';
 import Context from '../context/Context';
 
 function CategoryButtons({ categoryName }) {
   const [dataMealsCategories, setDataMealsCategories] = useState([]);
   const [dataDrinksCategories, setDataDrinksCategories] = useState([]);
+  const [clickedButton, setClickedButton] = useState('');
   const MAX_LENGTH_CATEGORIES = 5;
   const mealsCategories = dataMealsCategories.slice(0, MAX_LENGTH_CATEGORIES);
   const drinksCategories = dataDrinksCategories.slice(0, MAX_LENGTH_CATEGORIES);
@@ -18,10 +21,30 @@ function CategoryButtons({ categoryName }) {
     getCategoriaBebidas().then((response) => setDataDrinksCategories([...response]));
   }, []);
 
-  function onClickButton(categoryType, buttonName) {
-    return categoryType === 'Comidas'
+  function isNameDuplicated(category) {
+    const mainFetch = category === 'Comidas'
+      ? getBuscaComidas().then((response) => setDataMeals([...response]))
+      : getBuscaBebidas().then((response) => setDataDrinks([...response]));
+    return mainFetch;
+  }
+
+  function isntNameDuplicated(category, buttonName) {
+    const categoryFetch = category === 'Comidas'
       ? getFilteredMeals(buttonName).then((response) => setDataMeals([...response]))
       : getFilteredDrinks(buttonName).then((response) => setDataDrinks([...response]));
+    return categoryFetch;
+  }
+
+  function onClickButton(categoryType, buttonName) {
+    const isButtonDuplicated = clickedButton === buttonName;
+    if (isButtonDuplicated) {
+      setClickedButton('');
+    } else {
+      setClickedButton(buttonName);
+    }
+    return (isButtonDuplicated)
+      ? isNameDuplicated(categoryType)
+      : isntNameDuplicated(categoryType, buttonName);
   }
 
   function mapCategory(arrayToMap) {
