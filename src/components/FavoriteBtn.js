4ae4a997fabb718016400968b190739046b50e89
viniciusantana <viniciusantana@yahoo.com.br>
2022-01-09 21:24:43 -0300

    ajuste de conflito em favoriteBTN

--- src/components/FavoriteBtn.js
+++ src/components/FavoriteBtn.js
@@ -1,30 +1,25 @@
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import React, { useContext, useState } from 'react';
 import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import blackHeartIcon from '../images/blackHeartIcon.svg';
 import { isFavorited, setFavoritedItem } from '../service/localStorage';
 import Context from '../context/Context';
 
-export default function FavoriteBtn({ id }) {
-  const [favorite, setFavorite] = useState(isFavorited(id));
+export default function FavoriteBtn() {
   const { item } = useContext(Context);
-  console.log('favorite', favorite);
-  console.log(item, 'item');
+  const [favorite, setFavorite] = useState(isFavorited(item.idMeal));
+  console.log(item);
   return (
     <button
       type="button"
-      data-testid="favorite-btn"
       onClick={ () => { setFavoritedItem(item); setFavorite(!favorite); } }
     >
       <img
         src={ favorite ? blackHeartIcon : whiteHeartIcon }
         alt="HeartIcon"
+        data-testid="favorite-btn"
       />
       Favoritar
     </button>
   );
 }
-
-FavoriteBtn.propTypes = {
-  id: PropTypes.string.isRequired,
-};
